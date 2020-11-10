06c2c071f4de14b6d4853f4d74aa708740cc729b
Patrick <ptrollip@gmail.com>
2020-11-10 04:48:16 +0000

    Added a textarea for the sayings to display the random saying. I added a onclick event to the button. Started making a few global vars and function for generating a random saying in my script file.

---
+++ resources/custom.js
@@ -0,0 +1,7 @@
+let saying1 = "";
+
+const randomSayingMaker = () => {
+    let sayThis = document.getElementById("generatedSaying");
+    let buttonDown = document.addEventListener("newSaying");
+    if(buttonDown === true){};
+};
