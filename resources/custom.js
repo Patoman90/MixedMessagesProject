90da87e11a4a6b0eb1b74b60031a97f9e265a34a
Patrick <ptrollip@gmail.com>
2020-11-10 05:20:28 +0000

    Added variables, a new array called chooseSaying to function randomSayingMaker()

--- resources/custom.js
+++ resources/custom.js
@@ -1,7 +1,21 @@
-let saying1 = "";
+let saying1 = "Failure is part of achieving success.";
+let saying2 = "It will not always be good, but it will not always be bad.";
+let saying3 = "When you have a unreachable goal ahead, remember to look back.";
+let saying4 = "Not every path ends with a reward, but that doesn't mean you don't have to choose it.";
+let saying5 = "When the fear of change is so great it keeps us in the same place.";
+let saying6 = "When the only luxury we have is our own thoughts.";
+let saying7 = "We are only as strong as our dignity allows us.";
+let saying8 = "Mankind has nothing only when he lacks the will.";
+let saying9 = "When the goals we set are not ours and our success is not theirs.";
+let saying10 = "Your path is a mixture of your choices and hesitations.";
 
 const randomSayingMaker = () => {
+    const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10];
+    let i = chooseSaying.length;
+    const iterator = Math.Random(chooseSaying.values(i));
     let sayThis = document.getElementById("generatedSaying");
     let buttonDown = document.addEventListener("newSaying");
-    if(buttonDown === true){};
+    if(buttonDown === true){
+        sayThis.append.innerHTML = iterator;
+    };
 };
