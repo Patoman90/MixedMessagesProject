a108cffeb45ec7e489c919a91724df547c208736
Patrick <ptrollip@gmail.com>
2020-11-12 10:38:32 +0000

    Changed the button attribute by removing the onclick. I removed the outer function that was supposed to generate a new random word

--- resources/custom.js
+++ resources/custom.js
@@ -2,6 +2,10 @@ let saying1 = "Failure is part of achieving success.";
 let saying2 = "It will not always be good, but it will not always be bad.";
 let saying3 = "When you have a unreachable goal ahead, remember to look back.";
 let saying4 = "Not every path ends with a reward, but that doesn't mean you don't have to choose it.";
+let saying1 = "Failure is part of achieving success.";
+let saying2 = "It will not always be good, but it will not always be bad.";
+let saying3 = "When you have a unreachable goal ahead, remember to look back.";
+let saying4 = "Not every path ends with a reward, but that doesn't mean you don't have to choose it.";
 let saying5 = "When the fear of change is so great it keeps us in the same place.";
 let saying6 = "When the only luxury we have is our own thoughts.";
 let saying7 = "We are only as strong as our dignity allows us.";
@@ -9,16 +13,11 @@ let saying8 = "Mankind has nothing only when he lacks the will.";
 let saying9 = "When the goals we set are not ours and our success is not theirs.";
 let saying10 = "Your path is a mixture of your choices and hesitations.";
 
+let changeThis = document.querySelector("button");
 
-const randomSayingMaker = () => {
-    const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10];
-    const randomWord = Math.floor(Math.random() * chooseSaying.length);
-    const newSaying = chooseSaying[randomWord];
-
-
-    let changeThis = document.getElementById("#pickMe").addEventListener('click', sayThis);
-
-    let sayThis = (event) => { return changeThis.innerHTML= newSaying;};
-
-    return sayThis();
-};
+changeThis.addEventListener('click', () => {
+  const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10];
+  const randomWord = Math.floor(Math.random() * chooseSaying.length);
+  
+  document.getElementById('pickMe').innerHTML= chooseSaying[randomWord];
+});
