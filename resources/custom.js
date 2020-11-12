84e94ba8ab3553c2785bd6179fef99e24b79b5c5
Patrick <ptrollip@gmail.com>
2020-11-12 10:55:13 +0000

    Found duplicate vars and removed them. Then  moved chooseSaying and randomWord vars back to local scope to test if it works

--- resources/custom.js
+++ resources/custom.js
@@ -1,7 +1,4 @@
-let saying1 = "Failure is part of achieving success.";
-let saying2 = "It will not always be good, but it will not always be bad.";
-let saying3 = "When you have a unreachable goal ahead, remember to look back.";
-let saying4 = "Not every path ends with a reward, but that doesn't mean you don't have to choose it.";
+
 let saying1 = "Failure is part of achieving success.";
 let saying2 = "It will not always be good, but it will not always be bad.";
 let saying3 = "When you have a unreachable goal ahead, remember to look back.";
@@ -13,11 +10,11 @@ let saying8 = "Mankind has nothing only when he lacks the will.";
 let saying9 = "When the goals we set are not ours and our success is not theirs.";
 let saying10 = "Your path is a mixture of your choices and hesitations.";
 
-const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10];
-const randomWord = Math.floor(Math.random() * chooseSaying.length);
-
 let changeThis = document.querySelector("button");
 
 changeThis.addEventListener('click', () => {
+  const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10];
+  const randomWord = Math.floor(Math.random() * chooseSaying.length);
+
   document.getElementById('pickMe').innerHTML= chooseSaying[randomWord];
 });
