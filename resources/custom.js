0537d753daf66d9fa171ce44b4c9395333442ea6
Patrick Trollip <49972294+Patoman90@users.noreply.github.com>
2020-12-08 19:12:36 +0000

    Update custom.js
    Added two more sayings to the list of variables.

--- resources/custom.js
+++ resources/custom.js
@@ -14,6 +14,8 @@ let saying12 = "Truth is not what you want it to be; it is what it is, and you m
 let saying13 = "Well raised children listen to their parents.";
 let saying14 = "Respect is earned not a given.";
 let saying15 = "Keeping silent about our past will not make our future louder.";
+let saying16 = "Heaviest shackles are worn by people who are afraid of failing themselves.";
+let saying17 = "Comfort of familiarity may prevent us from any change.";
 
 //Variable that selects the html page button.
 let changeThis = document.querySelector("button");
@@ -22,7 +24,7 @@ let changeThis = document.querySelector("button");
 changeThis.addEventListener('click', () => {
 
   //Variable that creates a array that uses the sayings data as values.
-  const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10, saying11, saying12, saying13, saying14, saying15];
+  const chooseSaying = [saying1, saying2, saying3, saying4, saying5, saying6, saying7,saying8, saying9, saying10, saying11, saying12, saying13, saying14, saying15, saying16, saying17];
 
   //Variable that calculates a random value from the above array.
   const randomWord = Math.floor(Math.random() * chooseSaying.length);
